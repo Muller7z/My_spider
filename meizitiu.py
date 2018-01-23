ac324cd53ad848902ca6fec4065fde27c5ca7d46
Muller_cee <631502010132@mails.cqjtu.edu.cn>
2018-01-23 15:06:50 +0800

    修正了一个bug

--- meizitiu.py
+++ meizitiu.py
@@ -3,13 +3,7 @@ import requests
 from lxml import html
 import os
 
-
 headers = {"Referer":"http://www.mzitu.com/","User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"}
-
-dir_name = input("请输入文件夹的名字:  ")
-os.mkdir(dir_name) # 新建一个一级目录
-os.chdir("D:\Spider\%s"%dir_name)  # 进入一级目录
-url_input = input("请输入链接地址：")
 
 
 def taotu_url(url_target):
@@ -17,19 +11,22 @@ def taotu_url(url_target):
     try:
         rsp = requests.get(url_target,headers=headers).content
         elm = html.fromstring(rsp)
-        page_number = elm.xpath('//a[@class="page-numbers"]/text()')[-1]
+        page_number = elm.xpath('//a[@class="page-numbers"]/text()')
+        #print(page_number)
+        if page_number == []:
+            page_number = 1
+        else:
+            page_number = page_number[-1]
         print("此链接下有%s个页面待爬取" % page_number)
-        for i in range(page_number):
+        for i in range(int(page_number)):
             url = url_target+'page/%s/'%(i+1)
             print(url)
             response = requests.get(url,headers=headers).content
             elmt = html.fromstring(response)
-            url_list = elmt.xpath('//ul[@id="pins"]/li/a/@href')  # 解析url 我使用的是xpath
+            url_list = elmt.xpath('//ul[@id="pins"]/li/a/@href')
             print(url_list)
             for each in url_list:
-                #url_finder(each)
-                print("正在查找分页")
-
+                url_finder(each)
     except Exception as e:
         with open("Error_logging.txt","wb")as f:
             error = str(e).encode(encoding="utf-8")
@@ -45,7 +42,22 @@ def url_finder(url):
         totle_nummber = str(elm.xpath('//div[@class="pagenavi"]//a/span/text()')[-2])
         file_title = str(elm.xpath('//div[@class="main"]//h2/text()')[0])
         print(file_title)
-        downloader(url,totle_nummber,file_title)
+        num_list = [i for i in range(int(totle_nummber))]
+        downloader(url,num_list,file_title)
+
+        '''
+        divide_number = int(totle_nummber) // 3
+        num_list_X = [x for x in range(divide_number)]
+        num_list_Y = [y for y in range(divide_number,2*divide_number)]
+        num_list_Z = [z for z in range(2*divide_number,int(totle_nummber))]
+        #print(num_list_X,num_list_Y,num_list_Z)
+        X = Thread(downloader(url,num_list_X,file_title))
+        Y = Thread(downloader(url, num_list_Y, file_title))
+        Z = Thread(downloader(url, num_list_Z, file_title))
+        X.start()
+        Y.start()
+        Z.start()
+        '''
     except Exception as e:
         with open("Error_logging.txt","wb")as f:
             error = str(e)+"联接超时,url="+ url
@@ -55,10 +67,11 @@ def url_finder(url):
 
 def downloader(url,nummber,title):
     '''构造分页的url，定位到下载url，并保存到本地'''
+    os.mkdir("%s" % title)  # 新建一个二级目录
+    os.chdir('.\\%s' % title)  # 进入到二级目录
     try:
-        os.mkdir("%s" % title)  # 新建一个二级目录
-        os.chdir('D:\Spider\%s\%s' % (dir_name,title))  # 进入到二级目录
-        for i in range(int(nummber)):
+
+        for i in nummber:
             url_real = url+"/" + str(i + 1)  # 根据图片总数 构造分页的url
             rsp = requests.get(url_real, headers=headers).content
             elm = html.fromstring(rsp)
@@ -67,15 +80,22 @@ def downloader(url,nummber,title):
             print(str(img_title))
             with open(img_title, "wb")as f:
                 f.write(requests.get(download_url, headers=headers).content)
-                '''这里是请求下载url并且保存，返回的是防盗链警告图，referer该如何改才好呢？'''
-        os.chdir('D:\Spider\%s'%dir_name)   # 切换回一级目录
+
     except Exception as e:
         with open("Error_logging.txt","wb")as f:
             error = str(e)+"联接超时,url="+ url
             f.write(error.encode(encoding="utf-8"))  # 记录下错误日志
         print(e)
+    os.chdir('..')  # 切换回一级目录
 
 
 if __name__ == "__main__":
-    taotu_url(url_input)
+
+    dir_name = input("请输入一个用于保存图片的文件夹名:")
+    try:
+        os.mkdir(dir_name)  # 新建一个一级目录
+    except :
+        print("文件夹已经存在，将会保存至此文件夹")
+    os.chdir(".\\%s" % dir_name)  # 进入一级目录
+    taotu_url('http://www.mzitu.com/xinggan/')
 
