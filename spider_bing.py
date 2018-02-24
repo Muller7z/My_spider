42594d8a91d6e29c1f963e37b7bd7dd5eecda6ca
Muller <1184276183@qq.com>
2018-02-24 21:55:29 +0800

    XX

--- spider_bing.py
+++ spider_bing.py
@@ -3,12 +3,13 @@ from lxml import html
 import requests
 import os
 
+
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36"}
 
 
 def spider(pag_num):
-    pag_num += 1
-    url = 'https://bing.ioliu.cn/?p=%s' % pag_num  # 构建主页面的url
+
+    url = 'https://bing.ioliu.cn/?p=%s'%pag_num  # 构建主页面的url
     rsp = requests.get(url,headers=headers).content
     elm = html.fromstring(rsp)
     pag_url_list = elm.xpath('//a[@class="mark"]/@href')  # xpath解析出此页面下的图片的href地址
@@ -26,6 +27,7 @@ def img_url_finder(url_list, pag):
         #print(img_title)
         try :
             downloader(down_url, img_title)  # 这就是调用下载器了
+
         except Exception as e:
             print(e)
     print("完成下载第%s页" % str(pag))
@@ -47,6 +49,8 @@ if __name__ == "__main__":
     dir_name = "wallpaper_from_bing"
     os.mkdir(dir_name)
     os.chdir(".\\%s"%dir_name)
-    pag_number = input("请输入一个页数，将会从头开始下载壁纸至此页--->")
-    for i in range(int(pag_number)):
+    pag_number = int(input("请输入一个页数，将会从头开始下载壁纸至此页--->"))
+    for i in range(1,pag_number+1):
         spider(i)
+
+
